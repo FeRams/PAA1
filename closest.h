538186b5613d9abb2e9616bfe88f0d0b32a34a99
FeRams <fe.nw@hotmail.com>
2021-03-18 17:36:58 -0300

    parte comum

--- closest.h
+++ closest.h
@@ -19,4 +19,4 @@ float distance (Ponto* first, Ponto* second);
 
 float forca_bruta (Ponto* inputs, int size, Ponto** first, Ponto** second);
 
-float divisao_e_conquista (Ponto* inputs, int inicio, int fim, Ponto* first, Ponto* second);
+float divisao_e_conquista (Ponto* inputs, int inicio, int fim, Ponto** first, Ponto** second);
